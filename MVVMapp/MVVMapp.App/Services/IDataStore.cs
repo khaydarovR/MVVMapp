ebecba3a28b5500874486667186c25baeb733580
Khaydarov.R <Khaydarov.R@D3072-203-KRI>
2023-09-14 11:56:00 +0300

    fix interface

--- MVVMapp/MVVMapp.App/Services/IDataStore.cs
+++ MVVMapp/MVVMapp.App/Services/IDataStore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Windows.UI.Notifications;
 
 namespace MVVMapp.App.Services
 {
@@ -8,8 +9,8 @@ namespace MVVMapp.App.Services
     {
         Task<bool> AddItemAsync(T item);
         Task<bool> UpdateItemAsync(T item);
-        Task<bool> DeleteItemAsync(DateOnly? date);
-        Task<T?> GetItemAsync(DateOnly? date);
+        Task<bool> DeleteItemAsync(string id);
+        Task<T?> GetItemAsync(string id);
         Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
     }
 }
